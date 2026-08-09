de5f3b0ebda4e834521cb01d8340e7c0bf8f2ce4
Roshan Tiwari <roshantiwari3143@gmail.com>
2026-08-09 10:04:38 +0530

    feat: Added resend email api in place of nodemailer

--- backend/utils/sendOTPEmail.js
+++ backend/utils/sendOTPEmail.js
@@ -1,23 +1,26 @@
-const transporter = require("../config/mailTransporter.js");
+const { Resend } = require("resend");
 
-const sendOTPEmail = async (email, otp) => {
-  await transporter.sendMail({
-    from: process.env.EMAIL_USER,
-    to: email,
-    subject: "BlogBucket Email Verification OTP",
+const resend = new Resend(process.env.RESEND_API_KEY);
 
+async function sendOTP(email, otp) {
+  const { data, error } = await resend.emails.send({
+    from: "BlogBucket <onboarding@resend.dev>",
+    to: [email],
+    subject: "BlogBucket Email Verification",
     html: `
-      <h2>Welcome to BlogBucket</h2>
-
-      <p>Your One-Time Password (OTP) is:</p>
-
-      <h1 style="letter-spacing:5px;">${otp}</h1>
-
-      <p>This OTP will expire in <strong>5 minutes</strong>.</p>
-
-      <p>If you did not request this, please ignore this email.</p>
+      <h2>BlogBucket Email Verification</h2>
+      <p>Your OTP is:</p>
+      <h1>${otp}</h1>
+      <p>This OTP will expire in 5 minutes.</p>
     `,
   });
-};
 
-module.exports = sendOTPEmail;
+  if (error) {
+    console.log("Email sending error:", error);
+    throw new Error("Failed to send OTP email");
+  }
+
+  console.log("OTP email sent:", data.id);
+}
+
+module.exports = sendOTP;
