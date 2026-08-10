9d7d84a45075da2e28b3ca751d73c16b61404ed9
Roshan Tiwari <roshantiwari3143@gmail.com>
2026-08-10 17:24:17 +0530

    fix: Fixed mailTransport config from port 465 to 587

--- backend/config/mailTransporter.js
+++ backend/config/mailTransporter.js
@@ -2,13 +2,16 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
   family: 4,
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
 
 transporter.verify((error, success) => {
